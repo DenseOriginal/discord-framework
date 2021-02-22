b39c850ba1e0a264a722451e766da1d2b64839b3
Anders Kok <ak.kornerup@gmail.com>
2021-02-22 17:28:37 +0100

    Added ping as prefix

--- src/core/bootstrap.ts
+++ src/core/bootstrap.ts
@@ -7,6 +7,7 @@ import { constructor } from 'tsyringe/dist/typings/types';
 export interface BootstrapOptions {
   token: string;
   prefix: string;
+  usePingAsPrefix?: boolean;
   listners?: constructor<any>[];
 }
 
@@ -15,8 +16,20 @@ export function bootstrap(mainHandler: constructor<any>, options: BootstrapOptio
 
   const handler: HandlerClass = container.resolve(mainHandler);
   client.on('message', (message: Message) => {
-    if (!message.content.startsWith(options.prefix)) return;
-    handler.run(message, message.content.slice(options.prefix.length));
+    // Quit if message doesn't start with prefix
+    // And options.usePingAsPrefix is false
+    if (!message.content.startsWith(options.prefix) && !options.usePingAsPrefix) return;
+
+    // Find the bot id
+    // Quit if message doesn't startwith botIdTag AND that we wan't to use a ping as prefix
+    // And message doesn't start prefix as we still wan't to use that
+    const botIdTag = '<@!' +  client.user?.id + '>';
+    if((!message.content.startsWith(botIdTag) && options.usePingAsPrefix) && !message.content.startsWith(options.prefix)) return;
+    
+    // Remove the prefix of botIdTag
+    const input = message.content.slice((message.content.startsWith(options.prefix) ? options.prefix : botIdTag).length);
+
+    handler.run(message, input.trim());
   });
 
   options?.listners?.forEach((constructor: constructor<ListenerClass>) => {
